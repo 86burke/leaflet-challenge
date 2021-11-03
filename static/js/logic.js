a765717265cdd38c0bcff9f64581320b5b70ffac
john burke <86burke@gmail.com>
2021-11-02 20:58:59 -0500

    FINAL COMMIT

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 var equakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 
 
-d3.json(equakeUrl, function(data) {
+d3.json(equakeUrl, function (data) {
 
   createFeatures(data.features);
   console.log(data.features)
@@ -42,7 +42,7 @@ function createFeatures(earthquakeData) {
 
 
   var earthquakes = L.geoJSON(earthquakeData, {
-    pointToLayer: function(earthquakeData, latlng) {
+    pointToLayer: function (earthquakeData, latlng) {
       return L.circle(latlng, {
         radius: radiusSize(earthquakeData.properties.mag),
         color: circleColor(earthquakeData.properties.mag),
@@ -64,29 +64,21 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
-  var grayscalemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.light",
-    accessToken: API_KEY
-  });
 
-  
   var faultLine = new L.LayerGroup();
-  
-  
+
+
   var baseMaps = {
-    "Greyscale Map": grayscalemap,
     "Satellite Map": satellitemap
   };
 
-  
+
   var overlayMaps = {
     Earthquakes: earthquakes,
     FaultLines: faultLine
   };
 
-  
+
   var myMap = L.map("map", {
     center: [
       37.09, -95.71
@@ -95,51 +87,51 @@ function createMap(earthquakes) {
     layers: [earthquakes, faultLine]
   });
 
- 
+
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
 
-  
+
   var faultlinequery = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json";
-  
-  
-  d3.json(faultlinequery, function(data) {
+
+
+  d3.json(faultlinequery, function (data) {
     L.geoJSON(data, {
-      style: function() {
-        return {color: "orange", fillOpacity: 0}
+      style: function () {
+        return { color: "orange", fillOpacity: 0 }
       }
     }).addTo(faultLine)
   })
 
-  
+
   function getColor(d) {
     return d > 5 ? '#ff3333' :
-           d > 4  ? '#ff6633' :
-           d > 3  ? '#ff9933' :
-           d > 2  ? '#ffcc33' :
-           d > 1  ? '#ffff33' :
-                    '#ccff33';
+      d > 4 ? '#ff6633' :
+        d > 3 ? '#ff9933' :
+          d > 2 ? '#ffcc33' :
+            d > 1 ? '#ffff33' :
+              '#ccff33';
   }
 
 
-  var legend = L.control({position: 'topright'});
-  
-  legend.onAdd = function (map) {
-  
-      var div = L.DomUtil.create('div', 'info legend'),
-          mags = [0, 1, 2, 3, 4, 5],
-          labels = [];
+  var legend = L.control({ position: 'topright' });
 
-          
-      for (var i = 0; i < mags.length; i++) {
-          div.innerHTML +=
-              '<i style="background:' + getColor(mags[i] + 1) + '"></i> ' +
-              mags[i] + (mags[i + 1] ? '&ndash;' + mags[i + 1] + '<br>' : '+');
-      }
-  
-      return div;
+  legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+      mags = [0, 1, 2, 3, 4, 5],
+      labels = [];
+
+
+    for (var i = 0; i < mags.length; i++) {
+      div.innerHTML +=
+        '<i style="background:' + getColor(mags[i] + 1) + '"></i> ' +
+        mags[i] + (mags[i + 1] ? '&ndash;' + mags[i + 1] + '<br>' : '+');
+    }
+
+    return div;
   };
-  
+
   legend.addTo(myMap);
 }
